05e845a605fa20c2a51cb7508138a27b073168a1
viet.dangquoc <ledat@smartwave247.com>
2024-05-09 11:44:44 +0700

    + update screw logic

--- Assets/Scripts/ScrewManager.cs
+++ Assets/Scripts/ScrewManager.cs
@@ -3,7 +3,7 @@
 public class ScrewManager : MonoBehaviour {
     Animator animator;
     static GameObject currentOutScrew = null;
-    bool isTouchingOutScrew = false; // Biến kiểm tra xem có chạm vào screw đang go out hay không
+    bool isTouchingOutScrew = false; 
 
     void Start() {
         animator = GetComponent<Animator>();
@@ -15,22 +15,18 @@ public class ScrewManager : MonoBehaviour {
                 if (touch.phase == TouchPhase.Began) {
                     Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
                     if (GetComponent<Collider2D>().OverlapPoint(touchPosition)) {
-                        // Kiểm tra nếu chạm vào screw đang go out
                         if (currentOutScrew == gameObject) {
-                            // Gọi hàm SetTrigger để screw đó go in
                             animator.SetTrigger("isGoIn");
-                            // Reset biến kiểm tra và lần chạm
+                            
                             isTouchingOutScrew = false;
                             currentOutScrew = null;
                         } else {
-                            // Kiểm tra nếu có screw đang go out và không phải là screw hiện tại
                             if (currentOutScrew != null && currentOutScrew != gameObject) {
                                 currentOutScrew.GetComponent<Animator>().SetTrigger("isGoIn");
                             }
                             if (currentOutScrew != gameObject) {
                                 animator.SetTrigger("isGoOut");
                                 currentOutScrew = gameObject;
-                                // Đặt biến kiểm tra thành true khi chạm vào screw đang go out
                                 isTouchingOutScrew = true;
                             }
                         }

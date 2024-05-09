933d0ebed6cfecf94ef27a1f0e26fa50f23ddb64
viet.dangquoc <ledat@smartwave247.com>
2024-05-09 10:21:34 +0700

    + logic of pulling out screw

---
+++ Assets/Scripts/AnimationStrings.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal class AnimationStrings {
+    internal static string isGoIn = "isGoIn";
+    internal static string isGoOut = "isGoOut";
+
+}
--- Assets/Scripts/ScrewManager.cs
+++ Assets/Scripts/ScrewManager.cs
@@ -1,16 +1,32 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ScrewManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class ScrewManager : MonoBehaviour {
+    Animator animator;
+    static GameObject currentOutScrew = null;
+
+    void Start() {
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (Input.touchCount > 0) {
+            foreach (Touch touch in Input.touches) {
+                if (touch.phase == TouchPhase.Began) {
+                    Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                    if (GetComponent<Collider2D>().OverlapPoint(touchPosition)) {
+                        // Kiểm tra nếu có screw nào khác đang go out, thì set trigger isGoIn cho chúng
+                        if (currentOutScrew != null && currentOutScrew != gameObject) {
+                            currentOutScrew.GetComponent<Animator>().SetTrigger("isGoIn");
+                        }
+
+                        // Nếu screw hiện tại chưa go out thì set trigger isGoOut cho nó
+                        if (currentOutScrew != gameObject) {
+                            animator.SetTrigger("isGoOut");
+                            currentOutScrew = gameObject;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
